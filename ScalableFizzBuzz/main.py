d7c6e4be002ed902ed90462354e621259c3d7ab5
Florian Burgener <florian.brgnr@gmail.com>
2025-06-20 14:17:41 +0200

    Update main.py

--- ScalableFizzBuzz/main.py
+++ ScalableFizzBuzz/main.py
@@ -1,8 +1,9 @@
 from functools import reduce
 
-VERSION = 1
+VERSION = 3
 N = 100
 
+print(f"Executing version {VERSION}...")
 rules = [{"number": 3, "text": "Fizz"}, {"number": 5, "text": "Buzz"}]
 
 match VERSION:
@@ -15,8 +16,8 @@ match VERSION:
                 print(*[rules[i]["text"] for i, x in enumerate(evals) if x], sep="")
             else:
                 print(i)
-    # Version 1.1:
-    case 11:
+    # Version 2:
+    case 2:
         for i in range(1, N + 1):
             evals = [i % x["number"] == 0 for x in rules]
 
@@ -24,10 +25,23 @@ match VERSION:
                 print(*[rules[i]["text"] for i, x in enumerate(evals) if x], sep="")
             else:
                 print(i)
-    # Version 2:
-    case 2:
+    # Version 3 (best version in terms of performance and readability):
+    case 3:
+        for i in range(1, N + 1):
+            output = ""
+
+            for rule in rules:
+                if i % rule["number"] == 0:
+                    output += rule["text"]
+            
+            if not output:
+                output += str(i)
+
+            print(output) 
+    # Version 4 (more compact version 3):
+    case 4:
         for i in range(1, N + 1):
             print("".join(r["text"] for r in rules if i % r["number"] == 0) or str(i))
-    # Version 3:
-    case 3:
+    # Version 5 (even more compact version 3):
+    case 5:
         print("\n".join([("".join(r["text"] for r in rules if i % r["number"] == 0) or str(i)) for i in range(1, N + 1)]))
