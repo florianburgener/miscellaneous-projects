f21cc0e4d71d6f3b553f44c601bb270346e7a744
Florian Burgener <florian.brgnr@gmail.com>
2025-06-24 02:34:48 +0200

    Refactoring + add README.md

---
+++ prime-numbers/README.md
@@ -0,0 +1,15 @@
+Number of runs used to calculate the average execution time : 20
+
+Maximum value of the largest prime number, if it exists: 1,000,000
+
+| Algorithm name        | Average execution time |
+|-----------------------|------------------------|
+| Algorithm 1           | 856.6059 ms            |
+| Algorithm 2           | 510.1347 ms            |
+| Sieve of Eratosthenes | 98.9218 ms             |
+
+* Algorithm 2 is faster than Algorithm 1 by a factor of 1.68.
+* Sieve of Eratosthenes is faster than Algorithm 2 by a factor of 5.16.
+* Sieve of Eratosthenes is faster than Algorithm 1 by a factor of 8.66.
+
+These results are inaccurate because other maximum values should be tested in order to reach a relevant conclusion. Taking this into account, it is clear that Sieve of Eratosthenes is the best algorithm, but it is very memory-intensive.
--- prime-numbers/main.py
+++ prime-numbers/main.py
@@ -1,24 +1,12 @@
 import math
 import time
 
-prime_numbers = []
+NUM_RUNS = 20
+MAX_PRIME_LIMIT = 1000000
 
 
+# This function does not expect to receive an even number.
 def is_prime_number_1(n):
-    n_sqrt = math.sqrt(n)
-
-    for x in prime_numbers:
-        if x > n_sqrt:
-            break
-
-        if n % x == 0:
-            return False
-
-    prime_numbers.append(n)
-    return True
-
-
-def is_prime_number_2(n):
     for x in range(3, int(math.sqrt(n)) + 1, 2):
         if n % x == 0:
             return False
@@ -26,7 +14,36 @@ def is_prime_number_2(n):
     return True
 
 
+# This algorithm checks all numbers between 3 and the square root of the number being evaluated.
+# Even numbers except 2 are skipped.
 def find_prime_numbers_1(n):
+    return [2] + [x for x in range(3, n + 1, 2) if is_prime_number_1(x)]
+
+
+# This function does not expect to receive an even number.
+def is_prime_number_2(n, memoization):
+    n_sqrt = math.sqrt(n)
+
+    for x in memoization:
+        if x > n_sqrt:
+            break
+
+        if n % x == 0:
+            return False
+
+    memoization.append(n)
+    return True
+
+
+# This algorithm keeps track of prime numbers that have already been found so that they can be used to find the next prime number more quickly,
+# since the i-th prime number can only be expressed using smaller prime numbers.
+def find_prime_numbers_2(n):
+    memoization = [2]
+    return [2] + [x for x in range(3, n + 1, 2) if is_prime_number_2(x, memoization)]
+
+
+# Sieve of Eratosthenes
+def find_prime_numbers_3(n):
     A = [True] * n
     A[0] = False
 
@@ -38,46 +55,29 @@ def find_prime_numbers_1(n):
                 A[j - 1] = False
                 j = j + i
 
-    return A
+    return [i + 1 for i, x in enumerate(A) if x]
 
 
 def main():
-    ZZZ = 10000000
+    algorithms = [
+        {"name": "Algorithm 1", "func": find_prime_numbers_1},
+        {"name": "Algorithm 2", "func": find_prime_numbers_2},
+        {"name": "Sieve of Eratosthenes", "func": find_prime_numbers_3},
+    ]
 
-    # A = find_prime_numbers_1(ZZZ)
-    # acc = 0
-    # largest_prime_number = None
+    for algorithm in algorithms:
+        total_execution_time = 0
 
-    # for i, x in enumerate(A):
-    #     if x:
-    #         acc += 1
-    #         largest_prime_number = i + 1
+        for _ in range(NUM_RUNS):
+            start_time = time.time()
+            prime_numbers = algorithm["func"](MAX_PRIME_LIMIT)
+            total_execution_time += time.time() - start_time
 
-    # return (acc, largest_prime_number)
-
-    # 2 is a prime number.
-    acc = 1
-    largest_prime_number = 2
-
-    for x in range(3, ZZZ + 1, 2):
-        if is_prime_number_1(x):
-            acc += 1
-            largest_prime_number = x
-
-    return (acc, largest_prime_number)
+        print(algorithm["name"] + ":")
+        print("  Number of prime numbers: " + str(len(prime_numbers)))
+        print(f"  Largest prime number less than or equal to {MAX_PRIME_LIMIT}: " + str(prime_numbers[-1]))
+        print(f"  Mean execution time ({NUM_RUNS} runs): %.4f ms" % (total_execution_time / NUM_RUNS * 1000))
 
 
 if __name__ == "__main__":
-    N = 1
-    acc = 0
-
-    for _ in range(N):
-        start_time = time.time()
-        (prime_numbers_found, largest_prime_number) = main()
-        acc += time.time() - start_time
-
-        prime_numbers = []
-
-    print(prime_numbers_found)
-    print(largest_prime_number)
-    print("--- %s seconds ---" % (acc / N))
+    main()
